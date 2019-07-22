12e57960988a0f09188bef90a606c1f1b7313f34
Romeo Enso <romeo.enso@viseo.com>
2019-07-22 12:06:42 +0800

    customer api

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -32,8 +32,61 @@ export async function save(req, res) {
   }
 }
 
-export async function getById(req, res) { }
+export async function getById(req, res) {
+  try {
+    const customer = await Customer.findById(req.params.id);
+    if (!customer) {
+      throw new Error('Sorry, we cannot find what you\'re looking for.');
+    }
+    const response = Object.assign({}, SUCCESS_MESSAGE, {
+      data: customer
+    });
+    res.send(response);
+  } catch (e) {
+    const response = Object.assign({}, ERROR_MESSAGE, {
+      message: e.message
+    });
+    return res.send(response);
+  }
+}
 
-export async function update(req, res) { }
+export async function update(req, res) {
+  try {
+    const customer = await Customer.findByIdAndUpdate(req.params.id,
+      req.body, {
+        new: true
+      });
 
-export async function deleteById(req, res) { }
+    if (!customer) {
+      throw new Error('Sorry, we cannot find your data.');
+    }
+    const response = Object.assign({}, SUCCESS_MESSAGE, {
+      data: customer
+    });
+
+    return res.send(response);
+  } catch (e) {
+    const response = Object.assign({}, ERROR_MESSAGE, {
+      message: e.message
+    });
+    return res.send(response);
+  }
+}
+
+export async function deleteById(req, res) {
+  try {
+    const customer = await Customer.findById(req.params.id);
+
+    if (!customer) {
+      throw new Error('Sorry, we cannot find your data.');
+    }
+    await Customer.findByIdAndRemove(req.params.id);
+    return res.send(SUCCESS_MESSAGE);
+
+  } catch (e) {
+    const errorReponse = Object.assign({}, ERROR_MESSAGE, {
+      message: e.message
+    });
+    return res.send(errorReponse);
+  }
+}
--- src/routes/Customer.js
+++ src/routes/Customer.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { isValidCustomer } from '../middlewares';
+import { isValidCustomer, isValidId } from '../middlewares';
 import { get, save, getById, update, deleteById } from '../controllers/CustomerController';
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:id', [isValidId], getById);
 router.post('/', [isValidCustomer], save);
+router.put('/:id', [isValidId, isValidCustomer], update);
+router.delete('/:id', [isValidId], deleteById);
 
 export default router;
